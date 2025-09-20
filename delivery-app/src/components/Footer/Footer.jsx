6d262af1b6fb7658a7cfd9b2ce154863ec860719
Hiwot32 <demissiehiwot32@gmail.com>
2025-09-20 20:33:27 +0300

    Footer created

--- delivery-app/src/components/Footer/Footer.jsx
+++ delivery-app/src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from './footerStyle.module.css'
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 function Footer() {
   return (
     <div className={styles.outerSection}>
         <div className={styles.container}>
             <div className={styles.lists}>
+                <div className={styles.media}>
                 <div className={styles.left}>
                     <h1>Foodie-Corner</h1>
                     <p className={styles.des}>Where culinary artistry meets doorstep convenience.
@@ -19,20 +24,21 @@ function Footer() {
                 <div className={styles.middle}>
                     <h2>Navigation</h2>
                     <ul>
-                        <li>Home</li>
-                        <li>Menu</li>
-                        <li>About Us</li>
-                        <li>Contact</li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact</a></li>
                     </ul>
 
+                </div>
                 </div>
                 <div className={styles.right}>
                     <h2>Social Connect</h2>
                     <ul>
-                        <li>facebook</li>
-                        <li>instagram</li>
-                        <li>X</li>
-                        <li>youtube</li>
+                        <li><a className={styles.face}href="#"><FacebookRoundedIcon /></a></li>
+                        <li><a className={styles.X}href="#"><XIcon /></a></li>
+                        <li><a className={styles.inst} href="#"><InstagramIcon /></a></li>
+                        <li><a className={styles.u} href="#"><YouTubeIcon /></a></li>
                     </ul>
 
                 </div>
@@ -40,8 +46,8 @@ function Footer() {
             
             <div className={styles.buttom}>
                 <hr />
-                <p>© 2025 Foodie-Frenzy. All rights reserved.</p>
-                <p>Designed by Hexagon Digital Services</p>
+                <p className={styles.btext}>© 2025 Foodie-Frenzy. All rights reserved.<br />
+                Designed by Hexagon Digital Services</p>
 
             </div>
         </div>
